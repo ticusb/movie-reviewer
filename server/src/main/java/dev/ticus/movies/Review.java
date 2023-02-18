4a73e870d09bf4646337dd7a0a982b2d38197c3b
Ticus Brandt <LeviticusB02@gmail.com>
2023-02-18 10:49:35 -0700

    Created a server and client folder, to keep organized

---
+++ server/src/main/java/dev/ticus/movies/Review.java
@@ -0,0 +1,26 @@
+package dev.ticus.movies;
+
+import org.bson.types.ObjectId;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Document(collection= "reviews")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Review {
+
+    @Id
+    private ObjectId id;
+
+    private String body;
+
+    public Review(String reviewBody) {
+        this.body = reviewBody;
+    }
+    
+}
