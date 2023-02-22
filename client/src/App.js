3a8fbcc69fb5f6ec5d3565e304bc08a774184208
Ticus Brandt <LeviticusB02@gmail.com>
2023-02-22 08:37:21 -0700

    Created Play button and centered it And trailer component

--- client/src/App.js
+++ client/src/App.js
@@ -5,6 +5,7 @@ import { useState, useEffect } from 'react';
 import Layout from './components/Layout.js';
 import Home from './components/home/Home.js'
 import Header from './components/header/Header';
+import Trailer from './components/trailer/Trailer.js';
 import { Routes, Route } from 'react-router-dom';
 
 
@@ -31,9 +32,9 @@ function App() {
       <Header />
       <Routes>
         <Route path='/' element={<Layout />}>
-          <Route path='/' element={<Home movies={movies} />}>
+          <Route path='/' element={<Home movies={movies} />}/>
+          <Route path='/Trailer/:ytTrailerId' element={<Trailer/>}/>
     
-          </Route>
         </Route>
       </Routes>
     </div>
--- client/src/components/hero/Hero.js
+++ client/src/components/hero/Hero.js
@@ -3,6 +3,9 @@ import React from 'react';
 import Carousel from 'react-material-ui-carousel';
 import { Paper } from '@mui/material';
 
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCirclePlay } from '@fortawesome/free-solid-svg-icons';
+
 
 const Hero = (props) => {
     let movies = []
@@ -18,6 +21,11 @@ const Hero = (props) => {
                                             </div>
                                             <div className='movie-title'>
                                                 <h4>{movie.title}</h4>
+                                            </div>
+                                            <div className='movie-buttons-container'>
+                                                <div className='play-button-icon-container'>
+                                                    <FontAwesomeIcon className='play-button-icon' icon={faCirclePlay}/>
+                                                </div>
                                             </div>
                                         </div>
                                     </div>
